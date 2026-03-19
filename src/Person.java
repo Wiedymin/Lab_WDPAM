38c7c1a4200f8e6d63e4c663917c0b49ba22cb31
Wiedymin <skarpetka102@gmail.com>
2026-03-19 17:06:56 +0100

    Add Employee.java

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,8 @@ public class Person {
     private int id;
     private String name;
     private String email;
+    private String name;
+    private String surname;
     public Person(int id, String name, String email) {
         this.id = id;
         this.name = name;
